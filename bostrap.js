9ee092abdbd3b00f02641f8cda2333b2fbbc1e22
ayamkotok-source <mijensudrajat@gmail.com>
2026-01-03 10:40:58 +0700

    Add files via upload

---
+++ bostrap.js
@@ -0,0 +1,5 @@
+function showContent(id) {
+        let contents = document.querySelectorAll('.content');
+        contents.forEach(content => content.classList.remove('active'));
+        document.getElementById(id).classList.add('active');
+    }
